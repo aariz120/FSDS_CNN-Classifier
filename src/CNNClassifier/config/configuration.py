5a253a56a925dc170ccf61061270a7b73fa8bead
“Aariz <aariz120@gmail.com>
2023-10-03 12:11:52 +0530

    completeing my first compnent

---
+++ src/CNNClassifier/config/configuration.py
@@ -0,0 +1,18 @@
+from CNNClassifier.utils.utils import read_yaml,create_directory
+from CNNClassifier.entity.config_entity import DataIngestionConfig
+from CNNClassifier.entity.config_entity import PrepareBaseModelConfig
+from CNNClassifier.entity.config_entity import TrainingConfig
+from CNNClassifier.entity.config_entity import EvaluationConfig
+from CNNClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
+from pathlib import Path
+import os
+
+class ConfigurationManager:
+     def __init__():
+          pass
+     
+     def get_data_ingestion_config():
+          pass
+     
+          
+          

f9c8c1557245cd700cc8bac1942e1c4d682213a3
“Aariz <aariz120@gmail.com>
2023-10-06 21:37:32 +0530

    new file update

--- src/CNNClassifier/__init__.py
+++ src/CNNClassifier/__init__.py
@@ -3,21 +3,20 @@ import sys
 import logging
 
 
-logging_str="[%(asctime)s: %(levelname)s: %(module)s]:%(message)s"
+logging_str="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 
 log_dir="logs"
 
-log_filepath=os.path.join(log_dir,exist_ok=True)
+log_filepath=os.path.join(log_dir,"running_logs.log")
 
-os.makedirs(log_dir,exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
     format=logging_str,
     handlers=[
         logging.FileHandler(log_filepath),
-        loging.StreamHandler(sys.stdout)
+        logging.StreamHandler(sys.stdout)
     ]
-                     )
-logger=logging.getLogger("CNNClassifier")
-
+                    )
+logger=logging.getLogger("CNNClassfier")

38d4fd7cc106ad82e3e07454bea291cea451e169
“Aariz <aariz120@gmail.com>
2023-10-07 00:18:34 +0530

    file upadted

--- src/CNNClassifier/pipeline/training.py
+++
@@ -1,8 +0,0 @@
-'''
-just assemble all the stages over here till training.
-
-write all the components at the single place for the single shot training
-
-in my .ipynb i have given all the running code
-
-'''
